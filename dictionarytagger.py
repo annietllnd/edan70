8725bffb5c6bddc49ab79658d5121a51f0727283
annietallund <annietallund@gmail.com>
2020-04-04 16:53:59 +0200

    cleanup, add comments to code

--- dictionarytagger.py
+++ dictionarytagger.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import os
 
+# given a dictionary, tokenize and return the title, abstract and corpus, respectively
 def load_article(article_dict):
 	article = []
 	paragraphs = []
@@ -27,32 +28,22 @@ def load_article(article_dict):
 		paragraph = [re.sub(r'[^\w\s]','',w).lower() for w in paragraph]
 		paragraphs.append(paragraph)
 
-
 	return title, abstract, paragraphs
 
-#loads a metadata file and puts the content into lists
+# loads a metadata file and puts the content into lists
 def load_metadata():
 	metadata = pd.read_csv("metadata_comm_use_subset_100.csv")
 	metadata = [[row] for row in metadata.values]
 
 	return metadata
 
+# currently not used
+#def load_metadata_row(cord_uid, metadata):
+#	for row in metadata:
+#		if(row[0][0] == cord_uid):
+#			return row
 
-	#for article in metadata:
-#		articles.append(article)#
-
-#	abstracts = [[entry[8].split()] for entry in metadata if isinstance(entry[8], str)]#
-
-#	for abstract in abstracts:
-#		abstract[0] = [re.sub(r'[^\w\s]','',w).lower() for w in abstract[0]] #
-
-#	return articles, abstracts
-
-def load_metadata_row(cord_uid, metadata):
-	for row in metadata:
-		if(row[0][0] == cord_uid):
-			return row
-
+# obtain the neccessary columns from the metadata given a row
 def obtain_metadata_args(row):
 	row = row[0]
 	cord_uid = row[0]
@@ -60,13 +51,16 @@ def obtain_metadata_args(row):
 	sourceid = row[5]
 	return cord_uid, sourcedb, sourceid
 
-#loads the dictionaries from the supplemental files provided by @Aitslab
+# loads the dictionaries from the supplemental files provided by @Aitslab
+# if another dictionary is used, specify the path in 'open'
 def load_dictionaries():
 	dictionary_virus = [row.strip() for row in open('Supplemental_file1.txt')]
 	dictionary_disease = [row.strip() for row in open('Supplemental_file2.txt')]
 
 	return dictionary_virus, dictionary_disease
 
+
+# tag a tokenized text using a given dictionary and return a list of the words
 def tag(dictionary, corpus):
 	tagged_words = []
 	for w in corpus:
@@ -74,24 +68,8 @@ def tag(dictionary, corpus):
 			tagged_words.append(w)
 
 	return tagged_words
-
-def tag_article(title, abstract, paragraphs, type, dictionary):
-
-	if(type == "main_body"):
-		for p in paragraphs:
-			p_words = tag(dictionary, p)
-			return p_words
-	elif(type == "abstract"):
-		for a in abstract:
-			a_words = tag(dictionary, a)
-			return a_words
-	elif(type == "title"):
-		t_words = tag(dictionary, title)
-		return t_words
-	else:
-		print("Invalid type")
-		return None
-
+# given a word and a corpus, find the match for that word
+# improve: what if there are several matches? 
 def get_span(match, body):
 	regex_match = r"(?i)\b({0})\b".format(match)
 	a = re.search(regex_match, body)
@@ -100,8 +78,7 @@ def get_span(match, body):
 	else:
 		return a.start(), a.end()
 
-
-
+# returns a pubannotation string
 def construct_annotation(corduid_text, sourcedb_text, sourceid_text, divid_index, main_text, denotations):
 	cord_uid = "\"cord_uid\":\"" + corduid_text + "\", "
 
@@ -124,6 +101,7 @@ def construct_annotation(corduid_text, sourcedb_text, sourceid_text, divid_index
 	body = "{" + cord_uid + sourcedb + sourceid + divid + text + project + denotations_str + "}"
 	return body
 
+# returns a denotation string for a single match
 def construct_dennoation(idd, begin, end, obj_url):
 	idd = "\"id\":\"" + idd + "\", "
 
@@ -134,6 +112,7 @@ def construct_dennoation(idd, begin, end, obj_url):
 	body = "{" + idd + span + obj + "}"
 	return body
 
+# returns a denotation string given a list of denotations, or just a placeholder string if it's empty
 def concat_denotations(den_list):
 	if(len(den_list) == 1):
 		return  "[" + den_list[0] + "]"
@@ -144,7 +123,7 @@ def concat_denotations(den_list):
 	first = True
 
 	for d in den_list:
-		if(d == den_list[-1]):
+		if(d == den_list[-1]): # if d is the last element
 			final_denotation += d
 		else:
 			final_denotation += d + ", "
@@ -152,6 +131,7 @@ def concat_denotations(den_list):
 
 	return "[" + final_denotation + "]"
 
+# exports a given annotation string to a file
 def export_pubannotation(id, section_index, type, annotation):
 	file_name = id + "-" + section_index + "-" + type
 	text_file = open("out/" + file_name + ".json", "wt")
@@ -162,15 +142,14 @@ def export_pubannotation(id, section_index, type, annotation):
 def main():
 	# find paths, can be separate
 	files_path = [os.path.abspath(x) for x in os.listdir('comm_use_subset_100')]
+	# splits the path so we will only obtain the actual file name
+	# improve: this is machine specific, would be better to have a general solution
+	# or a list of files
 	files_path = [path.split('edan70/edan70/') for path in files_path]
-	files_path = [path[1] for path in files_path]
-
+	files_path = [path[1] for path in files_path] # gets only the name of the file
 
 	# load dictionaries, can be separate
 	virus_dict, disease_dict = load_dictionaries()
-	disease_dict = disease_dict
-	disease_dict = disease_dict + ["sars"]
-	virus_dict = virus_dict + ["sars"]
 	dicts = [virus_dict, disease_dict]
 	dicts = {'Virus_SARS-CoV-2':virus_dict, 'Disease_COVID-19':disease_dict} #fix this
 
@@ -178,11 +157,12 @@ def main():
 	metadata = load_metadata()
 	cord_uids = [metadata[i][0][0] for i in range(len(metadata))]
 
-
-	for i in range(0,2): #exchange for range(len(files_path))
-		#load the ith file in the directory
+	for i in range(0,2): # improve: exchange for range(len(files_path)) for the real thing
+		#this index is used for the file name counter
 		divid_index = 0
-		path = 'comm_use_subset_100/' + files_path[i] #for loop
+
+		#load the ith file in the directory
+		path = 'comm_use_subset_100/' + files_path[i] 
 		with open(path, 'r') as f:
 			article_dict = json.load(f)
 
@@ -197,24 +177,24 @@ def main():
 		# sourceid_text:	metadata file
 		cord_uid, sourcedb, sourceid = obtain_metadata_args(metadata[i])
 
-		#obj_url 
+		# obj_url 
 		obj_url = metadata[i][0][16]
 
 		for c in corpus:
-			#obtain the original, untokenized text
+			# obtain the original, untokenized text 
 			if(c == 'title'):
 				section = [article_dict['metadata'][c]]
 			else:
 				section = [article_dict[c][0]['text'] for s in article_dict[c]]
-			for subc in section:
+			for subc in section: # iterate through each section that will have its own file
 				denotations = []	
 
 				for dictionary in dicts:
 					idd = dictionary
 					words = tag(dicts[dictionary], corpus[c])
-					#print("tag dictionary found", len(words), "matches: ", words)	
+					# print("tag dictionary found", len(words), "matches: ", words)	
 					if(words == []):
-						[begin, end] = ['-1', '-1']				
+						[begin, end] = ['-1', '-1']	#improve: maybe make a more sleek solution			
 					for w in words:
 						begin, end = get_span(w, section[0])
 						begin = str(begin)
@@ -228,8 +208,7 @@ def main():
 					
 				temp_divid = str(divid_index)
 				annotation = construct_annotation(cord_uid, sourcedb, sourceid, temp_divid, section[0], final_denotation)	
-				print(annotation)
-				divid_index = divid_index + 1
+				divid_index = divid_index + 1 # we want the index in each file to increase
 				export_pubannotation(cord_uid, temp_divid, c, annotation)
 
 
