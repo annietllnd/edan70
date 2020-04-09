00b5ceea08a49882be1924baf4962391063f5058
Sofi <sofi.flinck@gmail.com>
2020-04-09 15:55:18 +0200

    Cleaned code, refactoring, functionality for adding all matches in a text

--- dictionarytagger.py
+++ dictionarytagger.py
@@ -26,7 +26,6 @@ TODO-list:
     - Why we need "path = path[1:]" in find sha.
     - Find good solution for keeping track of metadata index.
     - Check correct columns retrieved!
-    - Merge tag and returning indices
 '''
 
 import os
@@ -35,13 +34,66 @@ import re
 import copy
 import pandas as pd
 
-
+DIRECTORY_NAME = 'comm_use_subset_100'
 PUNCTUATION_REGEX = r'[^\w\s]'
 METADATA_HEADER = ['cord_uid', 'sha', 'source_x', 'title', 'doi',
                    'pmcid', 'pubmed_id', 'license', 'abstract',
                    'publish_time', 'authors', 'journal', 'microsoftap_id',
-                   '_', 'has_pdf_parse', 'has_pmc_xml_parse', 'full_text_file'
-                   'url']
+                   '_', 'has_pdf_parse', 'full_text_file', 'url']
+
+
+def load_vocabularies():
+    """
+    Return dictionary of imported vocabularies lists proviced by p@Aitslab.
+    """
+    virus_vocab_list = [row.strip() for row in
+                        open('Supplemental_file1.txt')]
+    disease_vocab_list = [row.strip() for row in 
+                          open('Supplemental_file2.txt')]
+    vocabs_col_dict = {'Virus_SARS-CoV-2':
+                       virus_vocab_list,
+                       'Disease_COVID-19':
+                       disease_vocab_list}
+    return vocabs_col_dict
+
+
+# Global so we do not have to send it between methods.
+VOCABS_COL_DICT = load_vocabularies()
+
+
+def load_metadata():
+    """
+    Returns list with metadata and dictionary with sha as keys and indices
+    of metadata list as values.
+    """
+    metadata_csv_path = 'metadata_comm_use_subset_100.csv'
+    metadata_frame = pd.read_csv(metadata_csv_path,
+                                 na_filter=False,
+                                 names=METADATA_HEADER,
+                                 engine='python')
+    metadata = metadata_frame.to_dict('records')
+    index = 0
+    metadata_indices_dict = dict()
+    for data in metadata:  # TODO other solution?
+        shas = data['sha'].split('; ', 1)
+        for sha in shas:                                # TODO Do we need to
+            metadata_indices_dict.update({sha: index})  # keep track of indice
+        index += 1                                      # of page-id/sha?
+    return metadata, metadata_indices_dict
+
+
+def generate_tokens_dict(article_dict, regex=PUNCTUATION_REGEX):
+    """
+    Return dicionary of tokenized sections.
+    """
+    title = clean_title(article_dict, regex)
+    abstract = clean_abstract(article_dict, regex)
+    body_text = clean_body_text(article_dict, regex)
+
+    tokens_dict = {'title': title,
+                   'abstract': abstract,
+                   'body_text': body_text}
+    return tokens_dict
 
 
 def clean_title(data_dict, regex):
@@ -93,211 +145,182 @@ def clean_body_text(data_dict, regex):
     return paragraphs
 
 
-# Define function for tagging words in vocabulary list.
+def obtain_metadata_args(metadata_dict):
+    """
+    Returns necessary columns from metadata dictionary. Index 0 gives cord_uid,
+    index 1 gives source_x, index 2 gives pmcid.
+    """
+    cord_uid = metadata_dict['cord_uid']
+    source_x = metadata_dict['source_x']  # TODO not sure which column
+    pmcid = metadata_dict['pmcid']
+    metadata_info = [cord_uid, source_x, pmcid]
+    return metadata_info
+
+
 def tag_tokens(vocabulary, tokens):
     """
     Return list of token strings found in dictionary.
     """
-    tagged_words = []
+    tagged_words = set()
     for token in tokens:
-        if token in vocabulary:         # TODO Maybe find another definition
-            tagged_words.append(token)  # than vocabulary?
+        if token in vocabulary:      # TODO Maybe find another definition
+            tagged_words.add(token)  # than vocabulary?
     return tagged_words
 
 
-def obtain_metadata_args(metadata_dict):
+def find_token_indices(token, section_text):
     """
-    Returns necessary columns from metadata dictionary.
-    """
-    cord_uid = metadata_dict['cord_uid']
-    pmcid = metadata_dict['pmcid']  # TODO not sure which column
-    pubmed_id = metadata_dict['pubmed_id']
-    return cord_uid, pmcid, pubmed_id
-
-
-def load_vocabularies():
-    """
-    Return dictionary of imported vocabularies lists proviced by @Aitslab.
-    """
-    virus_vocab_list = [row.strip() for row in
-                        open('Supplemental_file1.txt')]
-    disease_vocab_list = [row.strip() for row in 
-                          open('Supplemental_file2.txt')]
-    vocabs_col_dict = {'Virus_SARS-CoV-2':
-                       virus_vocab_list,
-                       'Disease_COVID-19':
-                       disease_vocab_list}
-    return vocabs_col_dict
-
-
-def find_token_index(token, text):
-    """
-    Returns a list of index placement for tokens in a text.
+    Returns a list of index placement for tokens in a section text.
     """
     regex_token_match = fr"(?i)\b({token})\b"
-    matches_list = re.findall(regex_token_match, text)
-    token_index_list = [[None, None]]
-    for match in matches_list:
-        token_index_list.append([match.start(), match.end()])
+    matches_iterator = re.finditer(regex_token_match, section_text)
+    token_index_list = []
+    for match in matches_iterator:
+        token_index_list.append([str(match.start()), str(match.end())])
     return token_index_list
 
 
-# returns a pubannotation string
-def construct_annotation(corduid_text, sourcedb_text, sourceid_text, divid_index, main_text, denotations):
-    cord_uid = "\"cord_uid\":\"" + corduid_text + "\", "
+def process_section(article_dict, tokens_dict, metadata_dict):
+    """
+    For each text section
+    """
+    file_index = 0  # TODO Check indices for file index and content index
+    metadata_info = obtain_metadata_args(metadata_dict)
+    for text_section in tokens_dict:
+        paragraph_index = 0
+        # obtain the original, untokenized text
+        if text_section == 'title':
+            unprocessed_texts = [article_dict['metadata'][text_section]]
+        else:
+            unprocessed_texts = [section['text'] for section in
+                                 article_dict[text_section]]
+        # Iterate through each section of unprocessed texts that will generate
+        # its own file
+        for unprocessed_text in unprocessed_texts:
+            denotation = obtain_denotation(tokens_dict,
+                                           text_section,
+                                           unprocessed_text,
+                                           metadata_dict['url'])
+            annotation = construct_pubannotation(metadata_info,
+                                                 paragraph_index,
+                                                 unprocessed_text,
+                                                 denotation)
+            print(annotation)
+            # export_pubannotation(metadata_info[0],
+            #                     file_index,
+            #                     text_section,
+            #                     annotation)
+            file_index += 1  # Increase with each file
+            paragraph_index += 1  # Increase with each paragraph
 
-    sourcedb = "\"sourcedb\":\"" + sourcedb_text + "\", "
 
-    sourceid = "\"sourceid\":\"" + sourceid_text + "\", "
+def obtain_denotation(tokens_dict, section, unprocessed_text, url):
+    """
+    Returns a denotation string, string with text where tokens matches
+    where found.
+    """
+    denotations = []
+    for vocabulary in VOCABS_COL_DICT:
+        token_index_list = []
+        found_tokens = tag_tokens(VOCABS_COL_DICT[vocabulary],
+                                  tokens_dict[section])
+        for found_token in found_tokens:
+            token_index_pairs = find_token_indices(found_token, unprocessed_text)
+            if bool(token_index_pairs):
+                token_index_list.append(token_index_pairs)
+        for token_index_pair in token_index_list:
+            begin, end = token_index_pair[0][0], token_index_pair[0][1]
+            denotations.append(
+                construct_denotation(vocabulary,
+                                     begin,
+                                     end,
+                                     url))
+    return concat_denotations(denotations)
 
-    divid = "\"divid\":" + divid_index + ", "
 
-    text = "\"text\":\"" + main_text + "\", "
+def construct_denotation(idd, begin, end, url):
+    """
+    Returns a string for a single match.
+    """
+    idd = "\"id\":\"" + idd + "\", "
+
+    span = "\"span\":{\"begin\":" + begin + "," + "\"end\":" + end + "}, "
+
+    obj = "\"obj\":\"" + url + "\""
+    denotation = "{" + idd + span + obj + "}"
+    return denotation
+
+
+def concat_denotations(denotations):
+    """
+    Returns a complete string of all seperate denotations in list parameter,
+    or and empty string if there where no elements in the list.
+    """
+    if not bool(denotations):
+        return "[]"
+
+    full_denotation = ''
+
+    for denotation in denotations:
+        if denotation == denotations[-1]:
+            full_denotation += denotation
+        else:
+            full_denotation += denotation + ", "
+    return "[" + full_denotation + "]"
+
+
+def construct_pubannotation(metadata_info, section_index, text, denotation):
+    """
+    Returns a string in pubannotation format.
+    """
+    cord_uid = "\"cord_uid\":\"" + metadata_info[0] + "\", "
+
+    source_x = "\"sourcedb\":\"" + metadata_info[1] + "\", "
+
+    pmcid = "\"sourceid\":\"" + metadata_info[2] + "\", "
+
+    divid = "\"divid\":" + str(section_index) + ", "
+
+    text = "\"text\":\"" + text + "\", "
 
     project = "\"project\":\"cdlai_CORD-19\", "
 
-    denotations_temp = ""
-    for d in denotations:
-        denotations_temp = denotations_temp + d
+    denotations_str = "\"denotations\":" + denotation
 
-    denotations_str = "\"denotations\":" + denotations_temp
+    return "{" + cord_uid + source_x + pmcid + divid + text + project + \
+           denotations_str + "}"
 
-    body = "{" + cord_uid + sourcedb + sourceid + divid + text + project + denotations_str + "}"
-    return body
 
-# returns a denotation string for a single match
-def construct_denotation(idd, begin, end, obj_url):
-    idd = "\"id\":\"" + idd + "\", "
-
-    span = "\"span\":{\"begin\":" + begin + "," + "\"end\":" + end  + "}, "
-
-    obj = "\"obj\":\"" + obj_url + "\""
-    
-    body = "{" + idd + span + obj + "}"
-    return body
-
-# returns a denotation string given a list of denotations, or just a placeholder string if it's empty
-def concat_denotations(den_list):
-    if(len(den_list) == 1):
-        return "[" + den_list[0] + "]"
-    elif(len(den_list) == 0):
-        return "[]"
-
-    final_denotation = ''
-
-    for d in den_list:
-        if(d == den_list[-1]):  # if d is the last element
-            final_denotation += d
-        else:
-            final_denotation += d + ", "
-
-    return "[" + final_denotation + "]"
-
-# exports a given annotation string to a file
-def export_pubannotation(idd, section_index, type_text, annotation):
-    file_name = idd + "-" + section_index + "-" + type_text
+def export_pubannotation(idd, file_index, section_name, annotation):
+    """
+    Export pubannotation string to a file.
+    """
+    file_name = idd + "-" + str(file_index) + "-" + section_name
     text_file = open("out/" + file_name + ".json", "wt")
     text_file.write(annotation)
     text_file.close()
-
-
-def load_metadata():
-    """
-    Returns list with metadata and dictionary with sha as keys and indices
-    of metadata list as values.
-    """
-    metadata_csv_path = 'metadata_comm_use_subset_100.csv'
-    metadata_frame = pd.read_csv(metadata_csv_path,
-                                 na_filter=False,
-                                 names=METADATA_HEADER,
-                                 engine='python')
-    metadata = metadata_frame.to_dict('records')
-    indice = 0
-    metadata_indices_dict = dict()
-    for data in metadata:  # TODO other solution?
-        shas = data['sha'].split('; ', 1)
-        for sha in shas:                                 # TODO Do we need to
-            metadata_indices_dict.update({sha, indice})  # keep track of indice
-        indice += 1                                      # of page-id/sha?
-    return metadata, metadata_indices_dict
-
-
-def generate_tokens_dict(article_dict, regex=PUNCTUATION_REGEX):
-    """
-    Return dicionary of tokenized sections.
-    """
-    title = clean_title(article_dict, regex)
-    abstract = clean_abstract(article_dict, regex)
-    body_text = clean_body_text(article_dict, regex)
-
-    tokens_dict = {"title": title,
-                   "abstract": abstract,
-                   "body_text": body_text}
-    return tokens_dict
-
-
-def tag_and_export(article_dict, tokens_dict, metadata_dict):
-    divid_index = 0  # TODO Check indices for file index and content index
-    cord_uid, pcmid, pubmed_id = obtain_metadata_args(metadata_dict)
-    for section in tokens_dict:
-        # obtain the original, untokenized text
-        if section == 'title':
-            unprocessed_texts = [article_dict['metadata'][section]]
-        else:
-            unprocessed_texts = [section['text'] for section in article_dict[c]]
-        for unprocessed_text in unprocessed_texts:  # iterate through each section that will have its own file
-            denotations = []
-            for vocabulary in VOCABS_COL_DICT:
-                found_tokens = tag_tokens(VOCABS_COL_DICT[vocabulary],
-                                          tokens_dict[section])
-                if found_tokens == []:
-                    [begin, end] = ['-1', '-1']  # No tokens found
-                for found_token in found_tokens:
-                    begin, end = find_token_index(found_token, unprocessed_text)  #TODO fix indice and tagging
-                    begin = str(begin)
-                    end = str(end)
-                    if begin != '-1':
-                        denotations.append(
-                            construct_denotation(vocabulary,
-                                                 begin,
-                                                 end,
-                                                 metadata_dict['url']))
-
-            final_denotation = concat_denotations(denotations)
-
-            temp_divid = str(divid_index)
-            annotation = construct_annotation(cord_uid,
-                                              pcmid,
-                                              pubmed_id,
-                                              temp_divid,
-                                              unprocessed_texts[0],
-                                              final_denotation)
-            divid_index = divid_index + 1  # increase with each file
-            export_pubannotation(cord_uid, temp_divid, section, annotation)
-
-
-VOCABS_COL_DICT = load_vocabularies()
 
 
 def main():
     """
     Main program.
     """
-    article_paths = os.listdir('comm_use_subset_100')
+    article_paths = os.listdir(DIRECTORY_NAME)
     metadata_list, metadata_indices_dict = load_metadata()
-
     for article_name in article_paths:
-        with open(article_name, 'r') as file_:
-            article_dict = json.load(file_)
-
-        # this index is used for the file name counter
+        if article_name == ".DS_Store":  # For MacOS users skip .DS_Store-file
+            continue                     # generated.
+        full_path = DIRECTORY_NAME + '/' + article_name
+        with open(full_path) as article:
+            article_dict = json.load(article)
+        print(article_name)
         tokens_dict = generate_tokens_dict(article_dict)
         # Finds indice of metadata that matches with sha of article_name
-        # without '.JSON' part.
+        # (without '.JSON' part.)
         metadata_indice = metadata_indices_dict[
             article_name.replace('.json', '')]
         metadata_dict = metadata_list[metadata_indice]
-        tag_and_export(article_dict, tokens_dict, metadata_dict)
+        process_section(article_dict, tokens_dict, metadata_dict)
 
 
 if __name__ == '__main__':
