09c24df8584982727d6b1bf290075f90d7111319
Sofi Flink <sofi.flinck@gmail.com>
2020-04-27 17:25:27 +0200

    Priorities solution, tag words ending in vir, tag non-compound words, plural versions of words

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # edan70
 ## by Sofi Flink, Annie Tallund
+
+# Dictionary tagger for COVID-19
+This is a dictionary tagger which uses pattern based search for matching words in articles and exports them in pub-annotations format.
+
+## Authors:
+Annie Tallind, Lund University, Faculty of Engineering
+Kaggle ID: atllnd
+Github ID: annietllnd
+
+Sofi Flink, Lund University, Faculty of Engineering
+Kaggle ID: sofiflinck
+Github ID: obakanue
+
+# Credit:
+Dictionaries where generated using golden- and silver-standard
+implemented by Aitslab.
+
+# Set up environment:
+*This implementation is built on LinuxOS and as such filepaths are system specific. If using Windows OS make sure to check the paths are written properly before running the code.*
+In order to run this program pandas have to be installed, which is a Python Data Analysis Library. 
+## Installing pandas with pip on Ubuntu:
+1. Open terminal.
+2. Enter following code snippets:
+```
+sudo apt-get install python3-pip
+sudo pip3 install pandas
+```
+## Installing pandas with apt-get on Ubuntu:
+1. Open terminal.
+2. Enter following code snippet:
+```
+sudo apt-get install python3-pandas
+```
+
+Now the code can be run writing the following code snippet in the terminal in the project:
+python3 dictionarytagger.py
--- dictionarytagger.py
+++ dictionarytagger.py
@@ -1,4 +1,4 @@
-'''
+"""
 Dictionary tagger for COVID-19
 
 Authors:
@@ -11,13 +11,16 @@ Authors:
     Github ID: obakanue
 
 Credit:
-    TODO
+    Dictionaries where generated using golden- and silver-standard implemented by Aitslab.
 
 TODO-list:
     (- Way of reaching files through github.)
     - Create notebook with program.
-    - Evaluate model (last)
-'''
+    - Evaluate model (last) see discord for more information.
+        * For every dictionary class
+        * Precision recall
+        * List with errors
+"""
 
 import os
 import json
@@ -31,10 +34,10 @@ PUNCTUATION_REGEX = r'[^\w\s]'
 Patterns:
 1. All words ending in 'vir' case insensitive in class 'chemical_antiviral'.
 """
-patterns = {'chemical_antiviral':
+PATTERNS_DICT = {'chemical_antiviral':
             r'(?i)\b\S*vir\b'
             }
-section_matches = dict()
+paragraph_matches = dict()
 
 
 def load_vocabularies():
@@ -58,8 +61,7 @@ VOCABS_COL_DICT = load_vocabularies()
 
 def load_metadata():
     """
-    Returns list with metadata and dictionary with sha as keys and indices
-    of metadata list as values.
+    Returns list with metadata and dictionary with sha as keys and indices of metadata list as values.
     """
     metadata_csv_path = 'metadata_comm_use_subset_100.csv'
     metadata_frame = pd.read_csv(metadata_csv_path,
@@ -78,8 +80,8 @@ def load_metadata():
 
 def obtain_metadata_args(metadata_dict):
     """
-    Returns necessary columns from metadata dictionary. Index 0 gives cord_uid,
-    index 1 gives source_x, index 2 gives pmcid.
+    Returns necessary columns from metadata dictionary. Index 0 gives cord_uid, index 1 gives source_x, index 2 gives
+    pmcid.
     """
     cord_uid = metadata_dict['cord_uid']
     source_x = metadata_dict['source_x']
@@ -90,100 +92,97 @@ def obtain_metadata_args(metadata_dict):
 
 def process_article(article_dict, metadata_dict):
     """
-    Process article for each section and generate pub annotations for export to file.
+    Process article for each section and paragraph and generate pub-annotations for export to file.
     """
     file_index = 0
     metadata_info = obtain_metadata_args(metadata_dict)
     sections = ['metadata', 'abstract', 'body_text']
     for section in sections:
-        section_matches.clear()
-        paragraph_index = 0
         if section == 'metadata':
-            section_texts = [article_dict[section]['title']]
-            section = 'title'
+            section_paragraphs = [article_dict[section]['title']]
         else:
-            section_texts = [section['text'] for section in
-                             article_dict[section]]
-        if bool(section_texts):
-            section_texts = ['']
-        for section_text in section_texts:
-            denotation, paragraph_index = get_denotation(section_text,
-                                                         metadata_dict['url'],
-                                                         paragraph_index)
-            annotation = construct_pubannotation(metadata_info,
-                                                 paragraph_index,
-                                                 section_text,
-                                                 denotation)
-            # export_pubannotation(metadata_info[0],
+            section_paragraphs = [section['text'] for section in
+                                  article_dict[section]]
+        if not bool(section_paragraphs):
+            section_paragraphs = ['']
+        paragraph_index = 0
+        for paragraph in section_paragraphs:
+            tag_paragraph(paragraph)
+            denotation = get_paragraph_denotation(metadata_dict['url'])
+            annotation = construct_pub_annotation(metadata_info,
+                                                  paragraph_index,
+                                                  paragraph,
+                                                  denotation)
+            #print(annotation)
+            # export_pub_annotation(metadata_info[0],
             #                    file_index,
             #                    section,
             #                    annotation)
+            paragraph_index += 1
         file_index += 1  # Increase with each file
 
 
-def get_denotation(section_text, url, paragraph_index):
+def tag_paragraph(paragraph):
     """
-    Returns a denotation string, string with text where token and pattern
-    matches where found.
+    For a paragraph, iterate through all vocabularies and patterns and tag using corresponding regex-pattern.
     """
-    denotations = []
+    paragraph_matches.clear()
     for vocabulary in VOCABS_COL_DICT:
         for word in VOCABS_COL_DICT[vocabulary]:
-            pattern = fr'(?i)\b{word}\b'
-            tag_section(pattern, section_text, url, denotations, vocabulary)
+            pattern = fr'(?i)\b{word}(es|s)?\b'
+            tag_pattern(pattern, paragraph, vocabulary)
 
-    for word_class in patterns:
-        tag_section(patterns[word_class], section_text, url, denotations, word_class)
-    paragraph_index += 1
-    return concat_denotations(denotations), paragraph_index
+    for word_class in PATTERNS_DICT:
+        tag_pattern(PATTERNS_DICT[word_class], paragraph, word_class)
 
 
-def tag_section(pattern, section_text, url, denotations, word_class):
+def tag_pattern(pattern, text, word_class):
     """
-    Finds all matches of a section for a pattern.
+    For a particular pattern, find matches in paragraph and add to 'paragraph_matches' dictionary, if match is
+    prioritized.
     """
-    matches = tag_pattern(pattern, section_text, word_class)
-    if bool(matches):
-        for match in matches:
-            begin, end = match.begin(), match.end()
-            denotations.append(construct_denotation(word_class,
-                                                    begin,
-                                                    end,
-                                                    url))
-
-
-def tag_pattern(pattern, section_text, word_class):
-    """Virus_SARS-CoV-2
-    Returns a list of index placement for matches found using
-    pattern in a section text.
-    """
-    matches = []
-    for match in re.finditer(pattern, section_text):
-        word_match = match.group(0)
-        is_priority = check_match_priority(pattern, match, word_class)
+    for match in re.finditer(pattern, text):
+        print(match)
+        is_priority = is_match_priority(pattern, match.group(0), word_class)
         if is_priority:
-            matches.append(match)
-            section_matches.update({word_match: word_class})
-    return matches
+            paragraph_matches.update({match: word_class})
 
 
-def check_match_priority(pattern, new_match, word_class):
-    for word_match in section_matches:
+def is_match_priority(pattern, new_word_match, word_class):
+    """
+    Checks priorites of tagging for vocabularies. For 'Virus_SARS-CoV-2' and 'Disease_COVID-19' if already pattern
+    matches with existing match in 'paragraph_matches' then only the longest match will be kept in the dictionary.
+    Returns 'True' if new match is to be added (prioritized).
+    """
+    for match in paragraph_matches:
+        word_match = match.group(0)
         if word_class == 'Virus_SARS-CoV-2' or word_class == 'Disease_COVID-19':
             prev_tagged = re.match(pattern, word_match)
             if prev_tagged:
-                longest_match = max(new_match, word_match, key=len)
-                if longest_match == new_match:
-                    del section_matches[word_match]
+                longest_match = max(new_word_match, word_match, key=len)
+                if longest_match == new_word_match:
+                    del paragraph_matches[match]
                     return True
                 return False
             return True
-        return True
+    return True
+
+
+def get_paragraph_denotation(url):
+    """
+    Constructs complete string denotation for a paragraph.
+    """
+    denotations = []
+    for match in paragraph_matches:
+        denotations.append(construct_denotation(paragraph_matches[match],
+                                                str(match.start()),
+                                                str(match.end()), url))
+    return concat_denotations(denotations)
 
 
 def construct_denotation(idd, begin, end, url):
     """
-    Returns a string for a single match.
+    Returns a string denotation for a single match.
     """
     idd = "\"id\":\"" + idd + "\", "
 
@@ -196,8 +195,9 @@ def construct_denotation(idd, begin, end, url):
 
 def concat_denotations(denotations):
     """
-    Returns a complete string of all separate denotations in list parameter,
-    or and empty string if there where no elements in the list.
+    Returns a complete denotation string of all separate denotations in
+    list parameter, or an empty string if there where no elements in the
+    list.
     """
     if not bool(denotations):
         return "[]"
@@ -212,9 +212,9 @@ def concat_denotations(denotations):
     return "[" + full_denotation + "]"
 
 
-def construct_pubannotation(metadata_info, section_index, text, denotation):
+def construct_pub_annotation(metadata_info, section_index, text, denotation):
     """
-    Returns a string in pubannotation format.
+    Returns a string in pub-annotation format.
     """
     cord_uid = "\"cord_uid\":\"" + metadata_info[0] + "\", "
 
@@ -234,11 +234,11 @@ def construct_pubannotation(metadata_info, section_index, text, denotation):
            denotations_str + "}"
 
 
-def export_pubannotation(idd, file_index, section_name, annotation):
+def export_pub_annotation(idd, file_index, section, annotation):
     """
-    Export pubannotation string to a file.
+    Export pub-annotation string to corresponding section file.
     """
-    file_name = idd + "-" + str(file_index) + "-" + section_name
+    file_name = idd + "-" + str(file_index) + "-" + section
     text_file = open("out/" + file_name + ".json", "wt")
     text_file.write(annotation)
     text_file.close()
@@ -252,7 +252,7 @@ def main():
     metadata_list, metadata_indices_dict = load_metadata()
     for article_name in article_paths:
         if article_name == ".DS_Store":  # For MacOS users skip .DS_Store-file
-            continue  # generated.
+            continue                     # generated.
         full_path = DIRECTORY_NAME + '/' + article_name
         with open(full_path) as article:
             article_dict = json.load(article)
