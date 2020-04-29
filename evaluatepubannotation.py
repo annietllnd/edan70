dec7d99685599856d69055a23000ee33769b3595
annietallund <annietallund@gmail.com>
2020-04-29 16:10:44 +0200

    create class EvaluatePubannotation

---
+++ evaluatepubannotation.py
@@ -0,0 +1,19 @@
+
+
+def EvaluatePubannotation(tagger_output, true_output): 
+	#load json -> dict
+	#true_positives = some_function() # number of true positives
+	#false_positives = some_other_function() # number of false positives
+	#true_negatives = other_function() # number of true negatives
+	#false_negatives = last_one() # number of false negatives
+
+	true_output_dictionary['denotations']['span'] == tagger_output_dictionary['denotations']['span']
+
+	def precision():
+		# Print the precision 
+		print("yeehaw")
+
+	def recall():
+		# Print the recall
+		print("waheey")
+
